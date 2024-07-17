2664ac6f1d6fd0666bea984b16743e4668286a22
Eric Xu <xydxydxyd1@gmail.com>
2024-07-17 15:11:06 -0700

    Thinking about how to proceed with conflicting instructions

--- src/examine.py
+++ src/examine.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from pandasgui import show
 
-df = pd.read_pickle('data/responses_delimited.bin')
+#df = pd.read_pickle('data/responses_delimited.bin')
+df = pd.read_pickle('data/responses.bin')
 show(df)
