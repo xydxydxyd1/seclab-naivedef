4790f16d7eccc98a9e310d3bec69b4be6ba642a9
Eric Xu <xydxydxyd1@gmail.com>
2024-07-28 22:13:06 -0700

    Improved interface

--- position/judge/main.py
+++ position/judge/main.py
@@ -7,10 +7,13 @@ import numpy as np
 
 logger = logging.getLogger(__name__)
 
+
 def parse_args():
     parser = argparse.ArgumentParser(description="ChatGPT")
-    parser.add_argument("test_path", type=str, help="Path to the test pickle")
-    parser.add_argument("output_path", type=str, help="Path to save response pickle")
+    parser.add_argument("--test_path", type=str, help="Path to the test pickle",
+                        default="../data/gen_tests.pkl")
+    parser.add_argument("--output_path", type=str, help="Path to save response pickle",
+                        default="../data/judge.pkl")
     return parser.parse_args()
 
 
@@ -25,15 +28,20 @@ def judge(test_cases):
     match[match_2] = 2
     test_cases.insert(test_cases.shape[1], 'match', match)
 
+
 if __name__ == "__main__":
-    #logging.basicConfig(level=logging.INFO, filename='main.log', filemode='w')
+    logging.basicConfig(level=logging.INFO, filename='main.log', filemode='w')
     args = parse_args()
 
+    logger.info("Loading test cases from %s", args.test_path)
     test_cases = pd.read_pickle(args.test_path)
 
+    logger.info("Calling GPT")
     responses = call_gpt(test_cases.loc[:, 'full_prompt'], False, True)
     test_cases.insert(test_cases.shape[1], 'response', responses)
 
+    logger.info("Judging responses")
     judge(test_cases)
-    
+
+    logger.info("Saving test cases to %s", args.output_path)
     test_cases.to_pickle(args.output_path)
