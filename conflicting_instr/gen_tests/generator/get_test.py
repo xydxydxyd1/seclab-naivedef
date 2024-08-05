d39fe9bec7fcaab4c372ace3b4715f518c840055
Eric Xu <xydxydxyd1@gmail.com>
2024-08-05 15:46:25 -0700

    Fix import path

--- conflicting_instr/gen_tests/generator/get_test.py
+++ conflicting_instr/gen_tests/generator/get_test.py
@@ -1,11 +1,14 @@
 # Factory for Tests
 
-from position import PositionTest
+from generator.position import PositionTest
 
 test_dict = {
     'position': PositionTest,
     'format_capitalize': None
 }
 
+test_choices = test_dict.keys()
+
 def get_test(test_name):
     return test_dict[test_name]
+
